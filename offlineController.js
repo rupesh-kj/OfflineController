463030f0e8145953418e70f2b0fac1d18a3d44ed
Rupesh <128797048+rupesh-kj@users.noreply.github.com>
2024-12-06 11:19:53 +0530

    Update offlineController.js

--- offlineController.js
+++ offlineController.js
@@ -138,7 +138,7 @@ define([
   };
 
   const OBJ_INSPECTION = {
-    SCOPE: "(/resources/latest/WxInspection_c/)([0-9]*)$",
+    SCOPE: "^(?!.*createInspection).*WxInspection_c.*",
     STORE: "WxInspection",
     ID_FIELD: "Id",
   };
